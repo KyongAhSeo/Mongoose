ec450f47f07c750cc53ee31c5bed42821a401e70
DESKTOP-T0IK3GO\sluer <kyongahjulieseo@gmail.com>
2021-03-26 22:37:01 +0900

    Second commit

--- app.js
+++ app.js
@@ -1,59 +1,76 @@
-//Connect to mongoose
 const mongoose = require('mongoose');
 
-// Connection URL
 mongoose.connect('mongodb://localhost:27017/fruitsDB', {useNewUrlParser: true});
 
-//Insert a Document
-//Schema를 먼저 작성한다.
 const fruitSchema = new mongoose.Schema ({
-  name: String,
-  rating: Number,
+  name: {
+    type: String,
+	 //name을 적지 않았을 경우 에러 메시지 출력
+   required: [true, "Please check your data entry, no name specified!"]
+  },
+  rating: {
+    type: Number,
+		//1-10까지만 입력가능
+    min: 1,
+    max: 10
+  },
   review: String
 });
 
-//1번째 파라미터에는 컬렉션의 이름이 들어가는데 단수형태로 적어줘야한다.
-//하지만 mongoose가 이를 복수형태로 변환시켜서 실제로 데이터베이스를 들여다보면 복수형태로 바뀌어있다.
-//2번째 파라미터에는
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
-//fruitSchema를 바탕으로 만들어진 Fruit 모델에서 fruit 문서를 생성
 const fruit = new Fruit ({
   name : "Apple",
   rating : 10,
   review : "Great fruit"
 });
 
-//fruit 문서를 fruitsDB안의 Fruit 컬렉션에 저장
 //fruit.save();
 
-const kiwi = new Fruit ({
-  name : "Kiwi",
-  rating : 10,
-  review : "The best fruit"
+const personSchema = new mongoose.Schema ({
+  name: String,
+  age: Number,
+  favoriteFruit: fruitSchema
 });
 
-const orange = new Fruit ({
-  name : "Orange",
-  rating : 9,
-  review : "Nice fruit"
+const Person = mongoose.model("Person", personSchema);
+
+const pineapple = new Fruit({
+  name: "pineapple",
+  score: 9,
+  review: "The Best!"
 });
 
-//1번째 파라미터는 객체의 array가 들어간다.
-//2번재 파라미터는 콜백 함수가 들어간다.
-// Fruit.insertMany([kiwi, orange], function(err){
+const banana = new Fruit({
+  name: "banana",
+  score: 5,
+  review: "So So!"
+});
+
+// pineapple.save();
+// banana.save();
+//
+// Person.updateOne({name:"John"}, {favoriteFruit: banana}, function(err){
 //   if(err){
-//     console.log(err);
-//   }else{
-//     console.log("succesfully saved all the fruits to fruitsDB");
-//   }
-// });
+//       console.log(err);
+//     }else{
+//       console.log("Succesfully updated the document.");
+//     }
+//   });
+
+const person = new Person({
+  name: "John",
+  age: 37,
+  favoriteFruit: banana
+});
+
+// //person.save();
 
 Fruit.find(function(err, fruits){
   if(err){
     console.log(err);
   }else{
-
+		//document를 다 찾으면 db 연결을 자동으로 닫는다.
     mongoose.connection.close();
 
     fruits.forEach(function(fruit){
@@ -61,3 +78,11 @@ Fruit.find(function(err, fruits){
     });
   };
 });
+
+// Fruit.deleteOne({_id:"605ddee72b800e20f85980bf"}, function(err){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log("Succesfully deleted the document.");
+//   }
+// });
